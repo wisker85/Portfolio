ee636d1ae3f3346c6e03bcf6171eeb242543d7f7
wisker85 <gripunt@hotmail.com>
2024-02-07 16:20:06 +0100

    Portfolio in Svelte e skeleton ui, prima bozza con tailwind

---
+++ src/components/Header.svelte
@@ -0,0 +1,31 @@
+<script>
+    export let y;
+
+    let tabs = [
+        { name: 'Progetti', link: '#progetti' },
+        { name: 'Profilo', link: '#profilo' },
+    ];
+</script>
+
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid ' + (
+    y > 0 ? ' py-4 bg-slate-950 border-violet-950' : ' py-6 bg-transparent border-transparent'
+)}>
+    <h1 class="font-medium">
+        Andrea <b class="font-bold">Niccolini</b> 
+    </h1>
+    <div class="sm:flex items-center gap-4 hidden">
+        {#each tabs as tab}
+            <a href={tab.link} class="duration-200 hover:text-violet-600">
+                <p>{tab.name}</p>
+            </a>
+        {/each}
+    </div>
+    <a href="#" target="_blank" 
+        class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+        >
+        <div 
+            class="absolute top-0 right-full w-full h-full bg-violet-700 opacity-20 group-hover:translate-x-full z-0 duration-200"
+        />
+        <h4 class="relative z-9">Contattami</h4>
+    </a>
+</header>
